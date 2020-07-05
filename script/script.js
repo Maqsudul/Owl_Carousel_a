0428bbb735456dc1db32e817a78badd9f91df224
Maqsudul Karim <62174598+Maqsudul@users.noreply.github.com>
2020-07-05 20:52:57 +0600

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items: 3,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      dots: true,
+      autoplayHoverPause: true,
+      smartSpeed: 500
+  });
+});
